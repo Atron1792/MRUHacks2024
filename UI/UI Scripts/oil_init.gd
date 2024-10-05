8d98d42ceb518a59546f2ecf3bc3f7d4ac4f3ec2
Dev Bhatt <devbhaven@gmail.com>
2024-10-05 15:29:26 -0600

    Test Push

--- UI/UI Scripts/oil_init.gd
+++ UI/UI Scripts/oil_init.gd
@@ -17,10 +17,57 @@ var StoneInit = 0
 
 #Ready Initilization
 func _ready():
-	$"CanvasLayer/Oil Text".text = "Oil: " + str(OilInit)
-	$"CanvasLayer/Food Text".text = "Food: " + str(FoodInit)
-	$"CanvasLayer/Population Text".text = "Population" + str(PeopleInit)
-	$"CanvasLayer/Wood Text".text = "Wood: " + str(WoodInit)
-	$"CanvasLayer/Stone Text".text = "Stone: " + str(StoneInit)
+	$"CanvasLayer/Timer".start()
+
+	$"CanvasLayer/Oil Text".size.x = 100
+	$"CanvasLayer/Food Text".size.x = 100
+	$"CanvasLayer/Population Text".size.x = 130
+	$"CanvasLayer/Wood Text".size.x = 100
+	$"CanvasLayer/Stone Text".size.x = 100
+
+	#Text positioning (x,y)
+	$"CanvasLayer/Oil Text".position = Vector2(100,0)
+	$"CanvasLayer/Food Text".position = Vector2(200,0)
+	$"CanvasLayer/Population Text".position = Vector2(300,0)
+	$"CanvasLayer/Wood Text".position = Vector2(500,0)
+	$"CanvasLayer/Stone Text".position = Vector2(600,0)
+
+
 	
 	print(OilInit)
+func _process(delta):
+	resource_texts()
+
+
+func depletion_resource():
+	OilInit -= 5
+	FoodInit -= PeopleInit*0.5
+	if FoodInit > (PeopleInit*1.25):
+		PeopleInit += 1
+	elif FoodInit < (PeopleInit*0.99):
+		PeopleInit -= randi_range(0, .5)*PeopleInit
+
+func resource_texts():
+	$"CanvasLayer/Oil Text".text = "Oil: " + str(OilInit)
+	$"CanvasLayer/Food Text".text = "Food: " + str(FoodInit)
+	$"CanvasLayer/Population Text".text = "Population: " + str(PeopleInit)
+	$"CanvasLayer/Wood Text".text = "Wood: " + str(WoodInit)
+	$"CanvasLayer/Stone Text".text = "Steel: " + str(StoneInit)
+
+func add_Oil(Oil):
+	OilInit += Oil
+
+func add_Food(Food):
+	FoodInit += Food
+
+func add_Peps(Peps):
+	PeopleInit += Peps
+
+func add_Wood(Wood):
+	WoodInit += Wood
+
+func add_Steel(steel):
+	StoneInit += steel
+
+func _on_timer_timeout() -> void:
+	depletion_resource()
