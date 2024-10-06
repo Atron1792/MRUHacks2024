31289e65fd2e042ad3b31da91c3808e9d6502b29
Muhammad Abdullah <atron1792@gmail.com>
2024-10-06 05:47:17 -0600

    mistake?

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -9,7 +9,7 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://UserPOV.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
 
 func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/guide_menu.tscn")
